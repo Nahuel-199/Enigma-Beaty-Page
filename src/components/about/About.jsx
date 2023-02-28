c0b2623671aa8ecd191351f730d4211a79589b05
Nahuel Cernadas <gabrielnahuel96@gmail.com>
2023-02-28 10:30:38 -0300

    addimages

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../../context";
 import mq from "../../assets/fotoP.jpeg";
+import mq2 from "../../assets/parallax2.jpeg";
 import "./about.css";
 
 const About = () => {
@@ -25,12 +26,18 @@ const About = () => {
             sensibilidad y estrechez normal de las paredes vaginales y a la
             vulva
           </p>
-          <p className="about__description"style={{ color: darkMode && "#fff" }}>
-            ¿Cuáles son los beneficios que este tratamiento brinda, más allá de
+        </div>
+      </div>
+
+      <span className="section__subtitle">
+      ¿Cuáles son los beneficios que este tratamiento brinda, más allá de
             lo estético?
-          </p>
+      </span>
+      <div className="about__container container grid">
+        <img src={mq2} alt="about" className="about__img" />
+        <div className="about__data">
           <p className="about__description">
-            Una de sus cualidades más importantes es la de revertir la
+          Una de sus cualidades más importantes es la de revertir la
             incontinencia urinaria de esfuerzo de bajo grado, es decir, la
             pérdida de orina que ocurre al toser, reírse, estornudar o saltar. A
             su vez, mejora los síntomas genitourinarios frecuentes en
@@ -38,6 +45,7 @@ const About = () => {
             vaginales por la disminución de estrógenos. También aumenta la
             lubricación, brinda mayor sensibilidad en la vulva y disminuye la
             dispareunia (dolor durante el coito).
+            vulva
           </p>
         </div>
       </div>
