8097696341bb157e2f3b39873dd91008d0040be7
Nahuel Cernadas <gabrielnahuel96@gmail.com>
2023-03-02 14:02:21 -0300

    finish

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useContext } from 'react';
+import { ThemeContext } from "../../context";
 import logo from "../../assets/logopng1.png";
 import "./home.css";
 
 const Home = () => {
+  const theme = useContext(ThemeContext);
+    const darkMode = theme.state.darkMode;
+
   return (
-    <section className="home section" id="home">
+    <section style={{backgroundColor: darkMode && "#fff"}} className="home section" id="home">
       <h2 className="section__title title_home">Rejuvenecimiento Vaginal </h2>
-      <span className="section__subtitle title_home">
+      <span style={{color: darkMode && "gray"}}  className="section__subtitle title_home">
         Hiperlaxitud vaginal,
       </span>
-      <span className="section__subtitle subtitle_home">
+      <span style={{color: darkMode && "gray"}}  className="section__subtitle subtitle_home">
         Incontinencia Urinaria,
       </span>
-      <span className="section__subtitle subtitle_home">Difunción Sexual,</span>
-      <span className="section__subtitle subtitle_home">
+      <span style={{color: darkMode && "gray"}}  className="section__subtitle subtitle_home">Difunción Sexual,</span>
+      <span style={{color: darkMode && "gray"}}  className="section__subtitle subtitle_home">
         Revierte los sintomas asociados,
       </span>
-      <span className="section__subtitle subtitle_home">
+      <span style={{color: darkMode && "gray"}}  className="section__subtitle subtitle_home">
         a la menopausia (sequedad vaginal)
       </span>
       <img src={logo} alt="logo" className="logo-home" />
